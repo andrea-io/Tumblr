6c006206fae68907058f9ddd33a98c26023fcbd2
Andrea Gonzalez <andreagonz@fb.com>
2022-06-16 11:16:02 -0700

    Final Tumblr project

---
+++ Tumblr/postCell.h
@@ -0,0 +1,17 @@
+//
+//  postCell.h
+//  Tumblr
+//
+//  Created by Andrea Gonzalez on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface postCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/postCell.m
@@ -0,0 +1,29 @@
+//
+//  postCell.m
+//  Tumblr
+//
+//  Created by Andrea Gonzalez on 6/16/22.
+//
+
+#import "postCell.h"
+
+@implementation postCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //postCell *cell = [tableView dequeueReusableCellWithIdentifier:@"postCell" forIndexPath:indexPath];
+    
+    //return cell;
+//}
+
+@end
